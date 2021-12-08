453b41581503f538295302150733c1dcbed1bfd7
Andika <andika.kavin@gmail.com>
2021-12-08 21:10:42 +0700

    addUserUseCase

--- src/Applications/use_case/_test/AddUserUsecCase.test.js
+++ src/Applications/use_case/_test/AddUserUsecCase.test.js
@@ -2,16 +2,15 @@ const RegisterUser = require('../../../Domains/users/entites/RegisterUser');
 const RegisteredUser = require('../../../Domains/users/entites/RegisteredUser');
 const UserRepository = require('../../../Domains/users/UserRepository');
 const PasswordHash = require('../../security/PasswordHash');
-const AddUserUseCase = require('../AddUserUseCase');
+const AddUserUserUsecase = require('../AddUserUseCase');
 
-describe('AddUsercase', () => {
-  it('should orchestrating the add user action correctly', async () => {
+describe('AddUserUsecase', () => {
+  it('should orchesting the add user action correctly', async () => {
     const useCasePayload = {
-      username: 'dicodinsdag',
+      username: 'dicoding',
       password: 'secret',
-      fullname: 'asd asd',
+      fullname: 'asdasd asd',
     };
-
     const expectedRegisteredUser = new RegisteredUser({
       id: 'user-123',
       username: useCasePayload.username,
@@ -22,20 +21,20 @@ describe('AddUsercase', () => {
     const mockPasswordHash = new PasswordHash();
 
     mockUserRepository.verifyAvailableUsername = jest.fn()
-      .mockImplementation(() => Promise.resolve());
+      .mockImplementation(() => Promise.resolve);
     mockPasswordHash.hash = jest.fn()
       .mockImplementation(() => Promise.resolve('encrypted_password'));
     mockUserRepository.addUser = jest.fn()
       .mockImplementation(() => Promise.resolve(expectedRegisteredUser));
 
-    const getUserUseCase = new AddUserUseCase({
-      userrepository: mockUserRepository,
-      PasswordHash: mockPasswordHash,
+    const getUserUseCase = new AddUserUserUsecase({
+      userRepository: mockUserRepository,
+      passwordHash: mockPasswordHash,
     });
 
-    const registerUser = await getUserUseCase.execute(useCasePayload);
+    const registeredUser = await getUserUseCase.execute(useCasePayload);
 
-    expect(registerUser).toStrictEqual(expectedRegisteredUser);
+    expect(registeredUser).toStrictEqual(expectedRegisteredUser);
     expect(mockUserRepository.verifyAvailableUsername).toBeCalledWith(useCasePayload.username);
     expect(mockPasswordHash.hash).toBeCalledWith(useCasePayload.password);
     expect(mockUserRepository.addUser).toBeCalledWith(new RegisterUser({
